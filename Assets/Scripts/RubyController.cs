b5ece8a995ab8963a9f6903013a6d528abad5b51
Connor Wulf <connor.m.wulf@gmail.com>
2021-05-05 14:29:55 -0500

    initial commit

---
+++ Assets/Scripts/RubyController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector2 position = transform.position;
+        position.y += 3.5f * vertical * Time.deltaTime;
+        position.x += 3.5f * horizontal * Time.deltaTime;
+        transform.position = position;
+    }
+}
