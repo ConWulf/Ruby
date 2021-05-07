972f832f1fbdee2fa108f53a4f3fff0690917d93
Connor Wulf <connor.m.wulf@gmail.com>
2021-05-06 21:56:00 -0500

    adds health to ruby character

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,20 +4,36 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public int maxHealth = 5;
+    private int currentHealth;
+
+    private Rigidbody2D rigidBody2D;
+    private float horizontal;
+    float vertical;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Vector2 position = transform.position;
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
+    //FixedUpdate called at a fixed framerate; 
+    //used for directly influincing physics components
+    void FixedUpdate() {
+        Vector2 position = rigidBody2D.position;
         position.y += 3.5f * vertical * Time.deltaTime;
         position.x += 3.5f * horizontal * Time.deltaTime;
-        transform.position = position;
+        rigidBody2D.MovePosition(position);
+    }
+
+    void ChangeHealth(int amount) {
+        
     }
 }
