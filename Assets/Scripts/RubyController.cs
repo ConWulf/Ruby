a0499789e046f9321c8003d9d921c2746c9dac9a
Connor Wulf <connor.m.wulf@gmail.com>
2021-05-13 13:55:09 -0500

    adds projectile script and sprote

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -17,9 +17,12 @@ public class RubyController : MonoBehaviour
     Rigidbody2D rigidBody2D;
     float horizontal;
     float vertical;
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         rigidBody2D = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
     }
@@ -29,6 +32,17 @@ public class RubyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+        
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f)){
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
 
         if(isInvincible) {
             invincibleTimer -= Time.deltaTime;
@@ -49,6 +63,7 @@ public class RubyController : MonoBehaviour
 
     public void ChangeHealth(int amount) {
         if(amount < 0) {
+            animator.SetTrigger("Hit");
             if(isInvincible)
                 return;
 
